4f1b69e169e73d552640fd908cbfa204731e3a22
Alexander Bushukin <46033931+Protivsporta@users.noreply.github.com>
2022-04-16 00:21:01 +0300

    Fixed ownable issues

--- contracts/BSCToken.sol
+++ contracts/BSCToken.sol
@@ -3,19 +3,18 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BSCToken is ERC20, Ownable {
+contract BSCToken is ERC20 {
 
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
 
-    function mint(address _to, uint256 _amount) public onlyOwner {
+    function mint(address _to, uint256 _amount) public  {
         _mint(_to, _amount);
     }
 
-    function burn(address _to, uint256 _amount) public onlyOwner {
+    function burn(address _to, uint256 _amount) public  {
         _burn(_to, _amount);
     }
 }
